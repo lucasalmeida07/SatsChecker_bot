d044a1b6d7eecdbd7e0300e87a05e921ff5b625c
Name17 <92337335+lucasalmeida07@users.noreply.github.com>
2021-12-01 11:04:55 -0300

    First commit v0.1

---
+++ SatsPriceChecker_bot.py
@@ -0,0 +1,33 @@
+import telebot
+from Defs import *
+
+
+key = '2147013825:AAENiKfIl4l2YAzFlD1Z0rkIveJsInQkm3g'
+bot = telebot.TeleBot(key)
+
+
+def main():
+    @bot.message_handler(commands=['start'])
+    def greeting(message):
+        bot.send_message(message, 'Olá, seja bem vindo ao SatsChecker, seu Bot para consulta do preço do Bitcoin')
+
+    @bot.message_handler(commands=['showPrice'])
+    def showprice(message):
+        price = getbtcprice(incash=True)
+        if price != None:
+            bot.reply_to(message, f'O Bitcoin atualmente está sendo negociado por {price}')
+        else:
+            bot.reply_to(message, 'Não foi possível obter o preço do Bitcoin. Por favor, tente novamente mais tarde.')
+
+    @bot.message_handler(commands=['showSats'])
+    def showsats(message):
+        price = getsatsprice()
+        if price != None:
+            bot.reply_to(message, f'1 satoshi está sendo negociado hoje por R${getsatsprice()}')
+        else:
+            bot.reply_to(message, f'Não foi possivel obter o preço do satoshi. Por favor, tente novamente mais tarde')
+
+
+if __name__ == '__main__':
+    main()
+    bot.infinity_polling()
