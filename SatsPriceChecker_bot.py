890951fd0d40860458b6177d8412f9aaa7aec1f8
Name17 <92337335+lucasalmeida07@users.noreply.github.com>
2021-12-02 08:05:35 -0300

    Comando help e mudança de chamada dos outros comandos
    Adição do comando /help e mudança nos comandos /showPrice e /showSats, para minúsculas

--- SatsPriceChecker_bot.py
+++ SatsPriceChecker_bot.py
@@ -12,8 +12,24 @@ def main():
     @bot.message_handler(commands=['start'])
     def greeting(message):
         bot.send_message(message, 'Olá, seja bem vindo ao SatsChecker, seu Bot para consulta do preço do Bitcoin')
+        
+    
+  @bot.message_handler(commands=['help'])
+	def helping(message):
+		bot.reply_to(message, '''
+		O SatsChecker foi criado com a intenção de ajudar na consulta do preço do bitcoin em tempo real, realizando diversas funcionalidades de auxilio para quando você precisar. Esta é a versão alpha do bot, portanto, a 0.1 \o/. Mais funcionalidades serão adicionadas no decorrer do tempo e de acordo com a disponibilidade do desenvolvedor.
+		
+		Aqui vão os comandos disponíveis nesta versão:
+		/start -----	Retorna uma mensaguem de boas vindas
+		/help  -----	Retorna esta mensagem de ajuda
+		/showPrice --- Retorna o preço atual do Bitcoin(Por enquanto, utilizando apenas a cotação disponível pela Cointelegraph)
+		/showSats --- Retorna o preço atual de 1.000(mil) satoshis
 
-    @bot.message_handler(commands=['showPrice'])
+		Por enquanto apenas estes comandos estão disponíveis.
+		''')  
+    
+
+    @bot.message_handler(commands=['showprice'])
     def showprice(message):
         price = getbtcprice(incash=True)
         if price != None:
@@ -21,11 +37,11 @@ def main():
         else:
             bot.reply_to(message, 'Não foi possível obter o preço do Bitcoin. Por favor, tente novamente mais tarde.')
 
-    @bot.message_handler(commands=['showSats'])
+    @bot.message_handler(commands=['showsats'])
     def showsats(message):
         price = getsatsprice()
         if price != None:
-            bot.reply_to(message, f'1 satoshi está sendo negociado hoje por R${getsatsprice()}')
+            bot.reply_to(message, f'1000 satoshis estão sendo negociados hoje por R${getsatsprice()}')
         else:
             bot.reply_to(message, f'Não foi possivel obter o preço do satoshi. Por favor, tente novamente mais tarde')
 
